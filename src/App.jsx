f50e3c4851743d5797ea8a442d93ae926cf6bb64
marchingon12 <augustthegreat.cool@gmail.com>
2024-05-03 06:50:53 +0200

    refactor App to use pages
    Signed-off-by: marchingon12 <augustthegreat.cool@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import { ThemeProvider } from './components/ui/theme-provider';
 import Navbar from './components/Navbar'
-import NavMenu from './components/NavMenu'
-import { 
-  CarouselGreenstash, 
-  CarouselMyne, 
-  CarouselFeatured 
-} from './components/Carousel'
+import Footer from './components/Footer'
+import Home from './pages/Home';
+import NotFoundPage from './pages/NotFound';
+
 
 
 function App() {
@@ -16,22 +14,12 @@ function App() {
       <Router>
         <header>
           <Navbar />
-          {/* <NavMenu /> */}
         </header>
-        <main>
-          <h1>Pool of tears</h1>
-            <div className="card">
-              <h2 className='align-middle text-center' >Greenstash</h2>
-              <CarouselGreenstash />
-              <h2 className='align-middle text-center' >Myne</h2>
-              <CarouselMyne />
-              <h2>Featured in</h2>
-              <CarouselFeatured />
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-        </main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='*' element={<NotFoundPage />}/>
+        </Routes>
+        {/* <Footer />  */}
       </Router>
     </ThemeProvider>
   )
