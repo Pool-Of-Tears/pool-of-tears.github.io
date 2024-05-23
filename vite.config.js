b2f7877201e08449ebd480b2d9d7e1bf6efb9234
marchingon12 <augustthegreat.cool@gmail.com>
2024-05-23 03:07:42 +0200

    feat: Add rollup-plugin-visualizer for bundle analysis
    Signed-off-by: marchingon12 <augustthegreat.cool@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -15,6 +16,7 @@ export default defineConfig({
       '/fdroid': {
         target: 'https://f-droid.org',
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/fdroid/, '/api/v1'),
       },
       '/github': {
@@ -25,6 +27,18 @@ export default defineConfig({
     },
     watch: {
       usePolling: true,
+    },
+  },
+  build: {
+    rollupOptions: {
+      // Could potentially mess up CSS for production build
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
     },
   },
 });
