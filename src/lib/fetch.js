ac0b5e606b6572bd4b5889408ebbbe15ca1141b7
marchingon12 <augustthegreat.cool@gmail.com>
2024-05-23 22:23:56 +0200

    revert: Revert to proxy method for fdroid fetching
    Signed-off-by: marchingon12 <augustthegreat.cool@gmail.com>

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -73,11 +73,9 @@ export function FdroidFetch() {
   const [myneApkFdroidUrl, setMyneApkFdroidUrl] = useState('');
   const [greenStashApkFdroidUrl, setGreenStashApkFdroidUrl] = useState('');
 
-
   useEffect(() => {
     const fetchApkUrl = (packageName) => {
-      const baseURL = import.meta.env.MODE === 'production' ? 'https://f-droid.org/api/v1' : '/fdroid';
-      return axios.get(`${baseURL}/packages/${packageName}`).then((response) => {
+      return axios.get(`/fdroid/packages/${packageName}`).then((response) => {
         console.log('Response data:', response.data);
         const latestPackage = response.data;
         if (!latestPackage) {
